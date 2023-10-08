52600ee949d01313cc12292758ad7f77474728b2
JohnZacharis <63471924+JohnZacharis@users.noreply.github.com>
2023-10-08 21:00:07 +0300

    Update git.py

--- git.py
+++ git.py
@@ -86,4 +86,3 @@ if st.session_state.messages[-1]["role"] != "assistant":
 # Press the green button in the gutter to run the script.
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
