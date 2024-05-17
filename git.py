8cf7e122d09a3ceb63e504cf5966e33e07fc6306
JohnZacharis <63471924+JohnZacharis@users.noreply.github.com>
2024-05-17 15:32:15 +0300

    Update git.py

--- git.py
+++ git.py
@@ -1,10 +1,10 @@
---import streamlit as st
---from llama_index import VectorStoreIndex, ServiceContext, Document
---from llama_index.llms import OpenAI
---import openai
---from llama_index import SimpleDirectoryReader, load_index_from_storage,StorageContext
---import os
---import pickle
+import streamlit as st
+from llama_index import VectorStoreIndex, ServiceContext, Document
+from llama_index.llms import OpenAI
+import openai
+from llama_index import SimpleDirectoryReader, load_index_from_storage,StorageContext
+import os
+import pickle
 
 
 #import folium
